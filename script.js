904695e453599e190012a46a62ffaa81b4247ff6
Luis <bamcuent312@gmail.com>
2023-06-26 22:05:46 +0200

    actualizacion

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("abajo",window.scrollY>0);
 })
+
+
+const ul = document.querySelector("#nav__lista");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    ul.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    ul.classList.remove("visible");
+})
